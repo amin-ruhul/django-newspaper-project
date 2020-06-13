f67371c8fbe85a03909669738e3745ccc8634963
Ruhul Amin <ruhulinfo50@gmail.com>
2020-06-13 13:13:18 +0600

    updated

--- articles/urls.py
+++ articles/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path 
 
-from .views import ArticleListView,ArticleUpdateView,ArticleDetailsView,ArticleDeleteView
+from .views import ArticleListView,ArticleUpdateView,ArticleDetailsView,ArticleDeleteView,ArticleCreateView
 
 urlpatterns = [
     path('',ArticleListView.as_view(),name = 'article_list'),
     path('<int:pk>/edit/',ArticleUpdateView.as_view(),name = 'article_edit'),
     path('<int:pk>/',ArticleDetailsView.as_view(),name = 'article_details'),
     path('<int:pk>/delete/',ArticleDeleteView.as_view(),name = 'article_delete'),
+    path('new/',ArticleCreateView.as_view(),name = 'article_new'),
     
 ]
